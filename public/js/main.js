1c062d12e95f8e6dba03114056e77a0f34cf2f92
01Alekje <01Alexanderkjellberg@gmail.com>
2020-08-20 15:39:35 +0200

    Pulla denna!
    Vi har gjort länkande mellan bilder och knapptryck, samt lite smått interaktivt godis.

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,41 @@
 function autoHighlight(name_input) {
 
     if(name_input == null || name_input == ''){
-        return
+        return;
     }
 
-    var students = document.querySelectorAll('a.students');
+    var students = document.querySelectorAll('.students');
     
     for(a of students){
-        a.style.backgroundColor = 'transparent'
+        a.style.backgroundColor = 'transparent';
     }
 
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            answer = JSON.parse(this.response)
+            answer = JSON.parse(this.response);
             
             for(student of answer){
-                var element = document.getElementById(student.id)
+                var element = document.getElementById(student.id);
                 element.style.backgroundColor = 'rgba(255,0,0,0.4)';
             }
         }
     };
 
-    xhttp.open('POST' , `/search_name/${name_input}`)
+    xhttp.open('POST' , `/search_name/${name_input}`);
     xhttp.send();
 }
 
 function student_add(){
     var form = document.querySelector('aside.studentform');
     form.classList.toggle('show');
+}
+
+function showInfo(element) {
+    console.log(element);
+    var studentId = element.id;
+    var el = document.querySelector(".personImg");
+    console.log(el)
+    el.style.backgroundImage = `url('../img/students/${studentId}.jpg')`;
 }
